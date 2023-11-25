900cc3b2aaa0752e83a9b19d6148e6af7d8ce852
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-11-25 23:36:32 +0900

    새우아이콘 추가 + 주자창들 버튼탭 변경완료 / searchForm, Parking,  제작미완료

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,7 @@
-// screens/HomeScreen.js
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
+import { View, TextInput, StyleSheet, KeyboardAvoidingView, Platform, TouchableOpacity, Image  } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import { Button as RNEButton, Text, Icon } from 'react-native-elements';
 
 const HomeScreen = ({ navigation }) => {
   const [origin, setOrigin] = useState('');
@@ -23,19 +23,40 @@ const HomeScreen = ({ navigation }) => {
       <MapView
         style={styles.map}
         initialRegion={{
-          latitude: 36.3504, // 대전의 위도
-          longitude: 127.3845, // 대전의 경도
+          latitude: 36.3504,
+          longitude: 127.3845,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
       >
         {/* 출발지와 도착지에 마커 추가 */}
         {origin && <Marker coordinate={{ latitude: 36.3504, longitude: 127.3845 }} title="출발지" />}
-        {destination && <Marker coordinate={{ latitude: 36.3504, longitude: 127.3745}} title="도착지" />}
+        {destination && <Marker coordinate={{ latitude: 36.3504, longitude: 127.3745 }} title="도착지" />}
+        {/* Shrimp 이미지 표시 - 현재 자기 위치를 표시하는 아이콘*/}
+        <Marker coordinate={{ latitude: 36.3504, longitude: 127.3945 }}>
+          <Image source={require('../assets/shrimp.png')} style={{ width: 40, height: 40 }} />
+        </Marker>
       </MapView>
+
+
       {/* 출발지, 도착지 입력창과 버튼 */}
-      
-      <View style={styles.inputContainer}>
+        <View style={styles.inputContainer}>
+          {/* 주정차 금지구역, 유료주차장, 무료주차장 버튼 */}
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Icon name="ban" type="font-awesome" size={24} color="white" />
+            <Text style={styles.buttonText}>주정차 금지구역</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Icon name="dollar" type="font-awesome" size={24} color="white" />
+            <Text style={styles.buttonText}>유료주차장</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Icon name="hand-holding-usd" type="font-awesome" size={24} color="white" />
+            <Text style={styles.buttonText}>무료주차장</Text>
+          </TouchableOpacity>
+        </View>
+
         <TextInput
           style={styles.input}
           placeholder="출발지"
@@ -48,15 +69,14 @@ const HomeScreen = ({ navigation }) => {
           value={destination}
           onChangeText={(text) => setDestination(text)}
         />
-      </View>
-      
-      {/* 길찾기 시작 버튼 */}
-      <View style={styles.buttonContainer}>
-        <Button
-          title="길찾기 시작"
-          color="white"
+        {/* 길찾기 시작 버튼 */}
+         <TouchableOpacity
+          style={styles.routeButton}
           onPress={startNavigation}
-        />
+        >
+          <Text style={styles.routeButtonText}>길찾기 시작</Text>
+          <Icon name="directions" size={20} color="white" />
+        </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
   );
@@ -71,7 +91,6 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   inputContainer: {
-    flexDirection: 'column', // 세로로 나열
     justifyContent: 'center',
     alignItems: 'center',
     marginVertical: 20,
@@ -85,10 +104,35 @@ const styles = StyleSheet.create({
     width: '80%',
   },
   buttonContainer: {
-    width: '50%',
-    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginVertical: 10,
+  },
+  button: {
     backgroundColor: 'purple',
     borderRadius: 5,
+    padding: 10,
+    alignItems: 'center',
+    width: '30%',
+  },
+  buttonText: {
+    color: 'white',
+    marginTop: 5,
+  },
+  routeButton: {
+    backgroundColor: 'purple',
+    borderRadius: 5,
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+    justifyContent: 'center',
+  },
+  routeButtonText: {
+    color: 'white',
+    marginRight: 5,
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,20 +1,25 @@
-// screens/MapScreen.js
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet , Image   } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import ParkingCheckScreen from './ParkingCheckScreen';
 
+import { Icon } from 'react-native-elements';
 
 const MapTab = createBottomTabNavigator();
 
 const MapScreen = ({ route }) => {
-  const { origin, destination } = route.params;
+  // 일단 빈칸일 때 오류 방지를 위해 기본값 설정
+  const { origin = '', destination = '' } = route?.params || {};
+
+  // 가상의 현재 주소, 시간, 키로수 값
+  const currentAddress = "대전시 유성구";
+  const currentTime = "2023-11-22 15:30";
+  const currentDistance = "5 km";
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <MapView
-        style={{ flex: 1 }}
+        style={styles.map}
         initialRegion={{
           latitude: 36.3504, // 대전의 위도
           longitude: 127.3845, // 대전의 경도
@@ -25,16 +30,55 @@ const MapScreen = ({ route }) => {
         {/* 출발지와 도착지에 마커 추가 */}
         <Marker coordinate={{ latitude: 36.3504, longitude: 127.3845 }} title="출발지" />
         <Marker coordinate={{ latitude: 36.3504, longitude: 127.3745 }} title="도착지" />
+           {/* Shrimp 이미지 표시 - 현재 자기 위치를 표시하는 아이콘*/}
+        <Marker coordinate={{ latitude: 36.3504, longitude: 127.3945 }}>
+          <Image source={require('../assets/shrimp.png')} style={{ width: 40, height: 40 }} />
+        </Marker>
       </MapView>
-      <Text>출발지: {origin}</Text>
-      <Text>도착지: {destination}</Text>
-      {/* 여기에 현재 주소, 시간, 키로수를 표시하는 부분 추가 */}
+
+      {/* 현재 주소, 시간, 키로수 표시 부분 */}
+      <View style={styles.infoContainer}>
+        <View style={styles.infoItem}>
+          <Icon name="location-pin" type="entypo" size={24} color="black" />
+          <Text>{currentAddress}</Text>
+        </View>
+        <View style={styles.infoItem}>
+          <Icon name="clock" type="entypo" size={24} color="black" />
+          <Text>{currentTime}</Text>
+        </View>
+        <View style={styles.infoItem}>
+          <Icon name="road" type="font-awesome" size={24} color="black" />
+          <Text>{currentDistance}</Text>
+        </View>
+      </View>
+
+      {/* 하단 탭 네비게이션
       <MapTab.Navigator>
         <MapTab.Screen name="주정차확인" component={ParkingCheckScreen} />
         {/* 다른 탭 추가 */}
-      </MapTab.Navigator>
+      {/* </MapTab.Navigator> */} 
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+    width: '100%',
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingVertical: 10,
+    backgroundColor: 'white',
+  },
+  infoItem: {
+    alignItems: 'center',
+  },
+});
 
 export default MapScreen;
