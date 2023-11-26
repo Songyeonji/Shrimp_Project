61b377c1c6b43d3271ebbf8425cec6b0410a7f0e
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-11-26 19:21:57 +0900

    클릭시 금지구역, 유-무료 주차장 마커 표시

--- screens/ParkingCheckScreen.js
+++ screens/ParkingCheckScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -7,10 +7,21 @@ import { useNavigation } from '@react-navigation/native';
 
 
 const ParkingCheckScreen = () => {
-  // 가상의 현재 주소, 시간, 키로수 값
-  const currentAddress = '대전시 유성구';
-  const currentTime = '2023-11-22 15:30';
-  const currentDistance = '5 km';
+    const [showBanMarker, setShowBanMarker] = useState(true);
+    const [showDollarMarker, setShowDollarMarker] = useState(false);
+    const [showFreeMarker, setShowFreeMarker] = useState(false);
+
+    const toggleBanMarker = () => {
+      setShowBanMarker(!showBanMarker);
+    };
+
+    const toggleDollarMarker = () => {
+      setShowDollarMarker(!showDollarMarker);
+    };
+    const toggleFreeMarker = () => {
+      setShowFreeMarker(!showFreeMarker);
+    };
+  
 
   const reloadMap = () => {
     // TODO: 지도 리로드 로직 추가
@@ -33,21 +44,41 @@ const ParkingCheckScreen = () => {
           {/* shrimp.png의 경로를 실제 파일 경로로 바꿔야 합니다. */}
           <Image source={require('../assets/shrimp.png')} style={{ width: 40, height: 40 }} />
         </Marker>
+
+        
+        {showBanMarker && (
+          <Marker coordinate={{ latitude: 36.3604, longitude: 127.3945 }}>
+            <Icon name="ban" type="font-awesome" size={24} color="red" />
+          </Marker>
+        )}
+
+        {showDollarMarker && (
+          <Marker coordinate={{ latitude: 36.3704, longitude: 127.3945 }}>
+            <Icon name="dollar" type="font-awesome" size={24} color="green" />
+          </Marker>
+        )}
+
+        {showFreeMarker && (
+          <Marker coordinate={{ latitude: 36.3804, longitude: 127.3945 }}>
+            <Icon name="dollar-sign" type="font-awesome" size={24} color="blue" />
+          </Marker>
+        )}
+
       </MapView>
 
- 
+        
 
       {/* 주정차 관련 버튼들 */}
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button}onPress={toggleBanMarker}>
           <Icon name="ban" type="font-awesome" size={24} color="white" />
           <Text style={styles.buttonText}>주정차 금지구역</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={toggleDollarMarker}>
           <Icon name="dollar" type="font-awesome" size={24} color="white" />
           <Text style={styles.buttonText}>유료주차장</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={toggleFreeMarker}>
           <Icon name="dollar-sign" type="font-awesome" size={24} color="white" />
           <Text style={styles.buttonText}>무료주차장</Text>
         </TouchableOpacity>
